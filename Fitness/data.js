c817d26ab5496e236aaccb4b81c183aab8844a25
namtran.010 <hoang-nam.tran@ant-tech.eu>
2020-09-18 21:45:32 +0700

    App

---
+++ Fitness/data.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    day: 'Ngày 1',
+    morning: [
+      '150 bánh mì đen 180 calo',
+      '1 quả táo ăn kèm khoảng 80g (40 calo)',
+      '2 quả trứng gà (không lòng đỏ) luộc ( 40 calo)',
+    ],
+    lunch: [
+      '1 chén cơm 100g cơm (130 calo)',
+      'Rau củ quả',
+      'Tôm hấp 100g tôm (100 calo)',
+    ],
+    dinner: ['Khoai lang 100g (86 calo)'],
+    night: ['Salad và cá hồi (150g chứa 350 calo)'],
+    total: 'Tổng khoảng 1000 calo trong ngày',
+  },
+  {
+    day: 'Ngày 2',
+    morning: [
+      'Khoai lang luộc 100g. (90 calo)',
+      'Trứng chiên 2 quả (k lòng đỏ) 40 calo',
+      '2 lát bánh mì sanwich 200 calo',
+    ],
+    lunch: [
+      '1 chén cơm 100g cơm (130 calo)',
+      'Nấm các loại khoảng 100g',
+      '150g ức gà luộc k da (250 calo)',
+    ],
+    dinner: ['Trái cây, bắp, khoai lang, bánh gạo khoảng 100 calo'],
+    night: ['Salad tôm 100g (100 calo)', '1 lát bánh mì sanwich 100 calo'],
+    total: 'Tổng 1000 calo trong ngày',
+  },
+  {
+    day: 'Ngày 3',
+    morning: ['1 hủ tíu nam vang (400 calo)', 'Ăn nhiều rau'],
+    lunch: [
+      '1 chén cơm 100g cơm (130 calo)',
+      '70g măng tây',
+      '200g cá hồi chiên (chiên cùng với 30g dầu olive) 300 calo',
+    ],
+    dinner: ['Bánh mì đen 50g (60 calo)', 'Sinh tố chuối'],
+    night: [
+      'Salad trứng (2 quả trứng lấy 1 lòng đỏ 70 calo)',
+      '100g cá hồi hấp (150 calo)',
+    ],
+    total: 'Tổng khoảng 1100 calo trong ngày',
+  },
+];
